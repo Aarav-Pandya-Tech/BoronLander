e129afa8d66e00f8ad7fb5e36c87b83e1692aeb0
Aarav-Pandya-Tech <aaravpandya33@gmail.com>
2026-07-24 12:28:29 +0530

    fixed time error

--- main.py
+++ main.py
@@ -30,9 +30,9 @@ def update_time():
     time_current = time.strftime("%H:%M")
     timelabel.config(text=time_current)
     timelabel.after(1000, update_time)
-timelabel = Label(display,text="", font=("Helvetica", 53, "bold","italic"), fg="#2d4259", bg="#7ae7ff")
-timelabel.place(relx=0.5, y=124, anchor="center")
+timelabel = Label(display,text="", font=("Helvetica", 50, "bold","italic"), fg="#2d4259", bg="#7ae7ff")
+timelabel.place(relx=0.5, y=118, anchor="center")
 
-display.after(250, force_fullscreen)
+#display.after(250, force_fullscreen)
 update_time()
 display.mainloop()
