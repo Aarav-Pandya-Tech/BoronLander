b6f39eb990fde01a751897cd2ac2420309490c44
Aarav-Pandya-Tech <aaravpandya33@gmail.com>
2026-07-24 12:12:18 +0530

    fullscreen, exit button added

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ display = Tk()
 display.geometry("480x800")
 display.resizable(False,False)
 display.title("Boron Lander")
+display.attributes('-fullscreen', True)
 
 #The bg is set
 original_image = Image.open("boronbackg.jpg")
@@ -17,7 +18,12 @@ bg_image = ImageTk.PhotoImage(original_image)
 bg_label = Label(display, image=bg_image)
 bg_label.place(x=0, y=0, relwidth=1, relheight=1)
 
+edit_image = Image.open("editbutton.jpg")
+edittk_image = ImageTk.PhotoImage(edit_image)
 
+# Create the label placeholder for bg
+edit_button = Button(display, image=edittk_image,bg="#7ae7ff",command=display.destroy,relief="flat",activebackground="#7ae7ff")
+edit_button.place(x=0, y=0)
 # Time is displayed
 def update_time():
     time_current = time.strftime("%H:%M")
